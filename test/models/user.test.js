95f6340deed3da56af87bf69b6ebc6e3fba58fc8
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-19 10:49:48 -0700

    se modifico la prueba unitaria para que no le llegue un valor sin definir

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -2,12 +2,12 @@ const user = require('./../../app/modules/user') //llamamos aplicacion, que en e
 
 describe('Unit test for User Class', () => {
     test('Create an user object', () => {
-        const user1 = new user(1, "xian145", "Abraham Ariel", "Bio", "10/5/2018", "8/5/2022") //creamos el objeto con las cosas necesarias
+        const user1 = new user(1, "xian145", "Abraham Ariel", "Bio") //creamos el objeto con las cosas necesarias
         expect(user1.id).toBe(1)
         expect(user1.userName).toBe("xian145")
         expect(user1.name).toBe("Abraham Ariel")
         expect(user1.bio).toBe("Bio")
-        expect(user1.dateCreated).toBe("10/5/2018")
-        expect(user1.lastUpdate).toBe("8/5/2022")
+        expect(user1.dateCreated).not.toBeUndefined() //para segurarno que no este indefinido
+        expect(user1.lastUpdate).not.toBeUndefined() //para segurarno que no este indefinido
     })
 })
