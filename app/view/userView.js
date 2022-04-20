3878ff10ee5e0c83349b03ee099b0df6e3b18cdf
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-20 09:28:36 -0700

    se modifico el archivo para cumplir con el segundo requerimiento

--- app/view/userView.js
+++ app/view/userView.js
@@ -2,11 +2,16 @@ const userService = require("./../services/userService") //llamamos a la clase U
 
 class userView{
     static createUser (payload){
+        const keys =[`username`,'id','name']
         if (payload === null){
            return {error: "payload not found"}
         }
 
         else if (!(typeof payload.userName === 'string') || !(typeof payload.name === 'string') || !(typeof payload.id === 'number')) {
+            if (payload.userName === undefined || payload.name === undefined || payload.id === undefined){
+                return {error:"you need to fill all the fields"}
+            }
+            else
             return {error: "need to have a valid value"}
         }
     } 
