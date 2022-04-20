f12312a3b537f6084143c216300e32d23250c266
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-19 19:20:08 -0700

    se agrego una prueba unitaria

--- test/view/userView.test.js
+++ test/view/userView.test.js
@@ -2,9 +2,9 @@ const userView = require('./../../app/view/userView')
 
 describe("Test for userView", () => {
     test('1. Return an error object when try to create a new user with an null payload', () => {
-        const payload = null
-        const result = userView.createUser(payload)
+        const payload = null //se crea una variable con nada adentro
+        const result = userView.createUser(payload) //se manda el payload a la clase userView
 
-        expect(result.error).toMatch(/payload no existe/)
+        expect(result.error).toMatch(/payload no existe/) //compara que lo que le llegue sea lo mismo que lo que esta entre slash
     })
 })
