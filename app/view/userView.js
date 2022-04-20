045d371363ba4d1e0db3d22ce678bc068f279a4f
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-19 19:19:03 -0700

    se creo la clase userView que utiliza userServices para crea un objeto

--- app/view/userView.js
+++ app/view/userView.js
@@ -1,10 +1,9 @@
-const user = require("./../services/userService") //llamamos a la clase User
+const userService = require("./../services/userService") //llamamos a la clase User
 
 class userView{ //creacion de una nueva clase
     
     static createUser(payload) { //creamo un nuevo metodo estatico 
-        const newUser = new user(payload)
-        console.log(newUser);
+        return userService.create(payload)
     }
 
 }
