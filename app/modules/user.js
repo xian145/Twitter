e93219c7a9e95fc2e4f70be3398493542002a97e
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-19 19:17:27 -0700

    se agrego una una llave error condicional

--- app/modules/user.js
+++ app/modules/user.js
@@ -1,11 +1,12 @@
 class user {
-    constructor(id, userName, name, bio, dateCreated, lastUpdate) {
+    constructor(id, userName, name, bio) {
         this.id = id,
         this.userName = userName,
         this.name = name,
         this.bio = bio,
         this.dateCreated = new Date(),
-        this.lastUpdate = new Date()
+        this.lastUpdate = new Date(),
+        this.error = (this.id === null | this.userName === undefined | this.name === undefined)  ? "payload no existe" : "" //si se cumple con cualquiera de estas cosas se despliega el mensaje de que el payload no existe de lo contrario es un string vacio
     }
 
     get getUserName() {
@@ -31,7 +32,6 @@ class user {
     set setBio(newBio) {
         this.bio = newBio
     }
-
 }
 
 module.exports = user
